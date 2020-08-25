3748b99c6478908648567ae8ddec9a6e72ebf3a0
Andrew Wong <andrewclintonwong@gmail.com>
2020-08-24 23:08:48 -0700

    Add files via upload

---
+++ website/index.js
@@ -0,0 +1,20 @@
+$(window).on('load', function () {
+  appendLoadingDiv();
+});
+
+function appendLoadingDiv() {
+  let divs = document.querySelectorAll('.graph');
+  let sideBySide = document.querySelector("#ethnicity-side-by-side-graph");
+
+  for (i = 0; i < divs.length; i++) {
+    let loadingP = document.createElement('p');
+    loadingP.innerText = 'Loading...';
+    loadingP.className = 'loading';
+    divs[i].appendChild(loadingP);
+  }
+
+  let loadingP = document.createElement('p');
+  loadingP.innerText = 'Loading...';
+  loadingP.className = 'loading';
+  sideBySide.appendChild(loadingP);
+}
